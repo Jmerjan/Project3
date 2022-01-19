22a9d72eaf9f2723d91edbfa0109c1c2e364328a
unknown <you@example.com>
2022-01-18 22:56:09 -0500

    app.py

--- app.py
+++ app.py
@@ -14,15 +14,14 @@ from sqlalchemy.ext.automap import automap_base
 
 
 #database setup
-# rds_connection_string = "postgres:Korudo7$@localhost:5432/cad"
-# rds_connection_string = "postgres:m3t30rS4uru5!5m@localhost:5432/cad"
-rds_connection_string = "postgres:postgres@localhost:5050/cad"
+rds_connection_string = "postgres:Korudo7$@localhost:5432/cad"
+#rds_connection_string = "postgres:m3t30rS4uru5!5m@localhost:5432/cad"
 engine = create_engine(f'postgresql://{rds_connection_string}')
 Base = automap_base()
 Base.prepare(engine, reflect=True)
 #Base.classes.keys()
 print(Base.classes.keys())
-# Sentry = Base.classes.sentry
+Sentry = Base.classes.sentry
 Cad = Base.classes.cad
 Summary = Base.classes.summary
 Fireball = Base.classes.fire_ball_data
@@ -39,8 +38,9 @@ app = Flask(__name__)
 def home ():
     return render_template('index.html')
 
+
 ## route v and dis 
-@app.route("/stackedplot") 
+@app.route("/Cad") 
 def stackedplot ():
     stackedplotchart=session.query(Cad.v_rel, Cad.dist, Cad.cd, Cad.des)
 
@@ -56,42 +56,78 @@ def stackedplot ():
     jsonchartdata = jsonify(chartdata)
     return jsonchartdata
 
-# route summary information
+# route summary information 
 @app.route("/summary") 
 def summary ():
     summarychartinfo=session.query(Summary.energy, Summary.ip, Summary.mass, Summary.des, Summary.diameter)
-    summary_df = pd.DataFrame(summarychartinfo, columns=['energy', 'ip', 'mass', 'des', 'diameter'])
+    summary_df = pd.DataFrame(summarychartinfo, columns=['energy', 'ip', 'mass', 'des', 'diameter', 'h', 'darc','nobs', 'v_inf', 'first_obs', 'method', 'pdate', 'ndop', 'cdate', 'ps_cum', 'ndel', 'v_imp', 'ps_max', 'last_obs', 'fullname', 'n_imp', 'ts_max',' nsat'])
     summaryinfo =[]
-    for energy , ip, mass, des, diameter in summarychartinfo:
+    for energy , ip, mass, des, diameter, darc, h, nobs, v_inf, first_obs, method, pdate, ndop, cdate, ps_cum, ndel, v_imp, ps_max, last_obs, fullname, n_imp, ts_max, nsat  in summarychartinfo:
         cleaninfo={}
         cleaninfo['energy'] = energy
+        cleaninfo['darc'] = darc
         cleaninfo['ip']= ip
+        cleaninfo['h'] = h
+        cleaninfo['nobs']= nobs
         cleaninfo['mass'] = mass
-        cleaninfo['des'] = des
+        cleaninfo['v_inf']=v_inf
+        cleaninfo['first_obs']= first_obs
+        cleaninfo['method']= method
+        cleaninfo['pdate']= pdate
+        cleaninfo['ndop']= ndop
+        cleaninfo['cdate']= cdate
+        cleaninfo['ps_cum']= ps_cum
         cleaninfo['diameter'] = diameter
-
+        cleaninfo['ndel']= ndel
+        cleaninfo['v_imp']= v_imp
+        cleaninfo['ps_max']= ps_max
+        cleaninfo['lsat_obs']= last_obs
+        cleaninfo['fullname']= fullname
+        cleaninfo['n_imp']= n_imp
+        cleaninfo['ts_max']= ts_max
+        cleaninfo['nsat']= nsat
+        cleaninfo['des'] = des
         summaryinfo.append(cleaninfo)
     return jsonify(summaryinfo)
+# route gauge 
 
-
-
-# route map fire ball
-@app.route('/fireballmap')
+# route map fire ball altit , time 
+@app.route('/fireball')
 def fireball():
     fireballinfo=session.query(Fireball.date, Fireball.latitude, Fireball.latdirection, Fireball.longitude, Fireball.londirection)
-    fireball_df= pd.DataFrame(fireballinfo, columns=['date', 'latitude', 'latdirection', 'longitude', 'londirection'])
+    fireball_df= pd.DataFrame(fireballinfo, columns=['date','energy','time', 'latitude', 'latdirection', 'longitude', 'londirection', 'altitude_km'])
     fireballdata=[]
-    for date, latitude, latdirection, longitude, londirection in fireballinfo:
+    for date, energy, latitude, latdirection, longitude, londirection, time, altitude_km in fireballinfo:
         cleanfire={} 
         cleanfire['date'] = date
+        cleanfire['energy'] = energy
         cleanfire['latitude'] = latitude
         cleanfire['latdirection'] = latdirection
         cleanfire['longitude']= longitude
         cleanfire['londirection'] = londirection
+        cleanfire['time'] = time
+        cleanfire['altitude_km'] = altitude_km
         fireballdata.append(cleanfire)
     return jsonify(fireballdata)
 
-
+# route sentry 
+@app.route('/sentry')
+def sentry():
+    sentryinfo=session.query(sentry.id,sentry.energy, sentry.ip, sentry.ts, sentry.date, sentry.sigma_vi, sentry.ps, sentry.des)
+    sentry_df=pd.DataFrame(sentryinfo, columns=['id','energy','ip', 'ts', 'date', 'sigma_vi', 'ps', 'des'])
+    sentrydata=[]
+    for id, energy, ip, ts, date, sigma_vi, ps, des in sentryinfo:
+        cleansentry={}
+        cleansentry['id']= id
+        cleansentry['energy']= energy
+        cleansentry['ip']= ip
+        cleansentry['ts']=ts
+        cleansentry['date']=date
+        cleansentry['sigma_vi']= sigma_vi
+        cleansentry['ps']=ps
+        cleansentry['des']= des
+        sentrydata.append(cleansentry)
+    return  jsonify(sentrydata)
 
 
 if __name__ == "__main__":
